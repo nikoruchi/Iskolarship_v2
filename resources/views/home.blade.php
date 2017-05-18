4966d5f8e2b496ae8616a5e968ce876a3e724d26
XonGado <cjubs.delgado@gmail.com>
2017-05-18 14:42:28 +0800

    UI Improvements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 	<div class="container main-container">
 		<div class="row">
-			<div class="center-el col eight-height col-sm-6 col-sm-offset-3">
+			<div class="center-el col col-sm-6 col-sm-offset-3">
 				<form class="home-search">
 					<div class="input-group">
 						<input type="text" name="keyword" class="form-control" placeholder="Find the most suitable choice for you." />
@@ -12,13 +12,87 @@
 							<button class="btn btn-primary"><span class="glyphicon glyphicon-search"></span></button>
 						</div>
 					</div>
-					<div class="center-el">
+					<div class="center-el check-boxes">
 						<label class="checkbox-inline"><input type="checkbox" name="search_q"><i class="sub-check"><i class="check"></i></i>Scholarships</label>
 						<label class="checkbox-inline"><input type="checkbox" name="search_q"><i class="sub-check"><i class="check"></i></i>Sponsors</label>
 						<label class="checkbox-inline"><input type="checkbox" name="search_q"><i class="sub-check"><i class="check"></i></i>Scholars</label>
 						<label class="checkbox-inline"><input type="checkbox" name="search_q"><i class="sub-check"><i class="check"></i></i>Open Applications</label>
 					</div>
 				</form>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<ul class="search-results">
+					<li>
+						<!-- Image of the scholarship is placed here. -->
+						<!-- The H2 here is just a place holder -->
+						<h2 class="first-letter">S</h2>
+						<article>
+							<h2 class="name">Scholarship Name</h2>
+							<p class="desc">
+								This is a description of the scholarship. Anything entered here is just to fill in as to nothing is to be fetched or presented here. I don't know what I am saying.
+							</p>
+							<ul class="sample-grants">
+								<li>This is a sample grant.</li>
+								<li>Have 1000000 as your allowance.</li>
+								<li>Maximum of 3 grants to be previewed.</li>
+							</ul>
+							<a href="#" class="view">View</a>
+						</article>
+					</li>
+					<li>
+						<!-- Image of the scholarship is placed here. -->
+						<!-- The H2 here is just a place holder -->
+						<h2 class="first-letter">S</h2>
+						<article>
+							<h2 class="name">Scholarship Name</h2>
+							<p class="desc">
+								This is a description of the scholarship. Anything entered here is just to fill in as to nothing is to be fetched or presented here. I don't know what I am saying.
+							</p>
+							<ul class="sample-grants">
+								<li>This is a sample grant.</li>
+								<li>Have 1000000 as your allowance.</li>
+								<li>Maximum of 3 grants to be previewed.</li>
+							</ul>
+							<a href="#" class="view">View</a>
+						</article>
+					</li>
+					<li>
+						<!-- Image of the scholarship is placed here. -->
+						<!-- The H2 here is just a place holder -->
+						<h2 class="first-letter">S</h2>
+						<article>
+							<h2 class="name">Scholarship Name</h2>
+							<p class="desc">
+								This is a description of the scholarship. Anything entered here is just to fill in as to nothing is to be fetched or presented here. I don't know what I am saying.
+							</p>
+							<ul class="sample-grants">
+								<li>This is a sample grant.</li>
+								<li>Have 1000000 as your allowance.</li>
+								<li>Maximum of 3 grants to be previewed.</li>
+							</ul>
+							<a href="#" class="view">View</a>
+						</article>
+					</li>
+					<li>
+						<!-- Image of the scholarship is placed here. -->
+						<!-- The H2 here is just a place holder -->
+						<h2 class="first-letter">S</h2>
+						<article>
+							<h2 class="name">Scholarship Name</h2>
+							<p class="desc">
+								This is a description of the scholarship. Anything entered here is just to fill in as to nothing is to be fetched or presented here. I don't know what I am saying.
+							</p>
+							<ul class="sample-grants">
+								<li>This is a sample grant.</li>
+								<li>Have 1000000 as your allowance.</li>
+								<li>Maximum of 3 grants to be previewed.</li>
+							</ul>
+							<a href="#" class="view">View</a>
+						</article>
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
--- storage/framework/views/924482e0a94481880e1c0c76d67e8ef75b76686e.php
+++ storage/framework/views/924482e0a94481880e1c0c76d67e8ef75b76686e.php
@@ -1,30 +1,29 @@
-@extends('layouts.userTab')
-@section('content')
+<?php $__env->startSection('content'); ?>
 	<div class="container main-container">
 		<div class="row">
 			<div class="col-sm-8 col-sm-offset-2">
-				<img src="/image/{{ $user->user_imagepath }}.jpg" class="img-responsive user-pp img-circle"/>
-				<h1 class="user-name">{{ $student->student_fname }}</h1>
-				<h2 class="education"> {{ $student->student_studyfield }}, {{ $student->student_university }}</h2>
-				<h3 class="user-email">{{ $user->email }}</h3>
-				@if(Auth::user()->hasRole('student'))
+				<img src="/image/<?php echo e($user->user_imagepath); ?>.jpg" class="img-responsive user-pp img-circle"/>
+				<h1 class="user-name"><?php echo e($student->student_fname); ?></h1>
+				<h2 class="education"> <?php echo e($student->student_studyfield); ?>, <?php echo e($student->student_university); ?></h2>
+				<h3 class="user-email"><?php echo e($user->email); ?></h3>
+				<?php if(Auth::user()->hasRole('student')): ?>
 				<div class="btn-group flex">	
 					<a href="/Account Settings">		
 						<button class="btn btn-default"><span class="glyphicon glyphicon-cog"></span> Account Settings</button>
 					</a>
 				</div>
-				@endif
-				@if(Auth::user()->hasRole('sponsor'))
+				<?php endif; ?>
+				<?php if(Auth::user()->hasRole('sponsor')): ?>
 				<div class="btn-group flex">
 					<button class="btn btn-primary"> <span class="glyphicon glyphicon-envelope"></span> Message</button>
 				</div>
-				@endif
-				@if(Auth::user()->hasRole('student'))
+				<?php endif; ?>
+				<?php if(Auth::user()->hasRole('student')): ?>
 				<!-- <div class="row">
 					<div class="col-sm-6">
 						<div class="paper">
 							<h4 class="text-center youHave">You have</h4>
-							<h4 class="count">{{$scholarships}}</h4>
+							<h4 class="count"><?php echo e($scholarships); ?></h4>
 							<h4 class="text-center">Scholarships</h4>
 							<div class="flex"><button class="btn btn-default"><span class="glyphicon glyphicon-modal-window"></span> See all</button></div>
 						</div>
@@ -32,7 +31,7 @@
 					<div class="col-sm-6">
 						<div class="paper">
 							<h4 class="text-center youHave">You have</h4>
-							<h4 class="count">{{$sponsors}}</h4>
+							<h4 class="count"><?php echo e($sponsors); ?></h4>
 							<h4 class="text-center">Sponsors</h4>
 							<div class="flex">
 								<button class="btn btn-default"><span class="glyphicon glyphicon-modal-window"></span> See all</button>
@@ -42,7 +41,7 @@
 					<div class="col-sm-6">
 						<div class="paper margin-top">
 							<h4 class="text-center youHave">You have</h4>
-							<h4 class="count">{{$pending}}</h4>
+							<h4 class="count"><?php echo e($pending); ?></h4>
 							<h4 class="text-center">Pending Scholarships</h4>
 							<div class="flex">
 								<button class="btn btn-default"><span class="glyphicon glyphicon-modal-window"></span> See all</button>
@@ -60,7 +59,7 @@
 						</div>
 					</div>
 				</div> -->
-				@endif
+				<?php endif; ?>
 				<div>
 					<h2 class="text-center">Scholarships</h2>
 					<ul class="scholarships">
@@ -83,12 +82,13 @@
 		</div>
 	</div>
 
-@endsection
+<?php $__env->stopSection(); ?>
 
-@push('styles')
+<?php $__env->startPush('styles'); ?>
 	<link rel="stylesheet" type="text/css" href="css/scholar_page.css"/>
-@endpush
+<?php $__env->stopPush(); ?>
 
-@push('scripts')
+<?php $__env->startPush('scripts'); ?>
 	<script src="js/default_img.js"></script>
-@endpush
+<?php $__env->stopPush(); ?>
+<?php echo $__env->make('layouts.userTab', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
