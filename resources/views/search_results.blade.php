64be18d52125fa38cf64c26a30c75e4ab2774e74
AJ Calcaben <calcabenallynjoy@gmail.com>
2017-05-19 07:51:56 +0800

    changes done for search

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -3,7 +3,74 @@
 @section('content')
 
 	<div class="container main-container">
-		<h1 class="text-center">You have searched for: <span class="keyword">Something</span></h1>
+		{{ csrf_field() }}
+		<h1 class="text-center">You have searched for: <span class="keyword">{{$keyword}}</span></h1>
+		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+			
+				@if(!empty($scholarships))
+					@foreach($scholarships as $scho)
+					<ul class="search-results">
+						<li>
+							<!-- Image of the scholarship is placed here. -->
+							<!-- The H2 here is just a place holder -->
+							<h2 class="first-letter">S</h2>
+							<article>
+								<h2 class="name">{{ $scho->scholarship_name}}</h2>
+								<p class="desc">
+									{{ $scho->scholarship_desc }}
+								</p>
+								<ul class="sample-grants">
+									<li>Insert grant.</li>
+									<li>Insert another grant.</li>
+									<li>Insert another new grant.</li>
+								</ul>
+								<a href="#" class="view">View</a>
+							</article>
+						</li>
+					</ul>
+					@endforeach
+				@endif
+
+				@if(!empty($sponsors))
+					@foreach($sponsors as $spon)
+					<ul class="search-results">
+						<li>
+							<!-- Image of the scholarship is placed here. -->
+							<!-- The H2 here is just a place holder -->
+							<h2 class="first-letter">S</h2>
+							<article>
+								<h2 class="name">{{ $spon->sponsor_fname }} {{ $spon->sponsor_fname }}</h2>
+								<p class="desc"> {{ $spon->sponsor_job }} </p>
+								<p class="desc"> {{ $spon->sponsor_agency }} </p>
+								<a href="#" class="view">View</a>
+							</article>
+						</li>
+					</ul>
+					<p></p>
+					@endforeach
+				@endif
+
+				@if(!empty($scholars))
+					@foreach($scholars as $schol)
+					<ul class="search-results">
+						<li>
+							<!-- Image of the scholarship is placed here. -->
+							<!-- The H2 here is just a place holder -->
+							<h2 class="first-letter">S</h2>
+							<article>
+								<h2 class="name">{{ $schol->student_fname }} {{ $schol->student_lname }}</h2>
+								<p class="desc"> {{ $schol->student_studyfield }} </p>
+								<p class="desc"> {{ $schol->student_university }} </p>
+								<a href="#" class="view">View</a>
+							</article>
+						</li>
+					</ul>
+					@endforeach
+				@endif
+
+			</div>
+		</div>
 	</div>
 	
 @endsection
