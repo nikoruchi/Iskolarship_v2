66313729d90a09cb5176ef09fd27bf98b1a9ddb1
AJ Calcaben <calcabenallynjoy@gmail.com>
2017-05-23 07:49:44 +0800

    changes for search view profile

--- app/Http/Controllers/SponsorController.php
+++ app/Http/Controllers/SponsorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Sponsor;
+use App\Scholar;
 use App\User;
 use App\Scholarship;
 use Auth;
@@ -33,5 +34,38 @@ class SponsorController extends Controller{
 
         return view('profiles.profile_sponsor', compact('sponsor', 'user', 'scholarships'));       
     } 
+
+    public function viewSearchfromStudent($sponsor_id){
+        $user_id = Auth::user()->user_id;
+        $user = User::findOrFail($user_id);
+
+        $stud_id = Scholar::where('user_id','=', $user_id)->pluck('student_id');
+        $student = Scholar::findOrFail($stud_id);
+
+        $sponsor_id1 = Sponsor::where('sponsor_id','=', $sponsor_id)->pluck('sponsor_id');
+        $sponsor = Sponsor::find($sponsor_id1);
+
+        $scholarships = Scholarship::where('sponsor_id', "=", $sponsor_id1)->orderBy('scholarship_name')->get();
+
+ 
+
+        return view('profiles.profile_sponsor', compact('sponsor', 'student', 'user', 'scholarships'));       
+    } 
+
+    public function viewSearchfromSponsor($sponsor_id){
+        $user_id = Auth::user()->user_id;
+        $user = User::findOrFail($user_id);
+
+        $spon_id = Sponsor::where('user_id','=', $user_id)->pluck('sponsor_id');
+        $sponsor = Sponsor::findOrFail($spon_id);
+
+        $sponsor_id2 = Sponsor::where('sponsor_id','=', $sponsor_id)->pluck('sponsor_id');
+        $sponsor1 = Sponsor::find($sponsor_id2);
+
+        $scholarships = Scholarship::where('sponsor_id', "=", $sponsor_id2)->orderBy('scholarship_name')->get();
+
+        return view('profiles.profile_sponsor', compact('sponsor', 'sponsor1', 'user', 'scholarships'));       
+    } 
+
 
 }
